f7b12d561b92234445193a814ad972d28b26b1e9
Kowagatte <nnryanp@gmail.com>
2020-05-29 01:56:31 -0600

    Added AuthAccount
    Added AuthAccount, the object we will receive from the client when attempting to login.

--- src/main/kotlin/ca/damocles/storage/Account.kt
+++ src/main/kotlin/ca/damocles/storage/Account.kt
@@ -3,8 +3,30 @@ package ca.damocles.storage
 import com.google.gson.Gson
 import java.util.*
 
+data class AuthAccount(val username: String, val password: String){
+    override fun toString(): String = Gson().toJson(this)
+}
+
 data class Account(val uuid: UUID, var email: String, var username: String, var password: String): Storable{
-    override fun toString(): String {
-        return Gson().toJson(this)
-    }
+    override fun toString(): String = Gson().toJson(this)
+}
+
+fun fromJson(accountJson: String): Account {
+    return Gson().fromJson(accountJson, Account::class.java)
+}
+
+/**
+ * Take AuthAccount
+ * Check if an account exists with the username of the auth account
+ * if it does, compare passwords (BCrypt.checkpw)
+ * return a pair, (boolean, Account) (true if it passes and the corrosponding account), ( false if it doesnt pass and null for the account)
+ *
+ *
+ * Testing purposes:
+ * username: Nick
+ * password: 12345qwesd
+ *
+ */
+fun authenticateLogin(){
+    TODO("")
 }
