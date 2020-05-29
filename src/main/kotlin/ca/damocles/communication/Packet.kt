9ba1818212f97d3db90a9d6e741bf4558769e4ac
Kowagatte <nnryanp@gmail.com>
2020-05-29 01:55:49 -0600

    Added LoginPacket

--- src/main/kotlin/ca/damocles/communication/Packet.kt
+++ src/main/kotlin/ca/damocles/communication/Packet.kt
@@ -1,10 +1,18 @@
 package ca.damocles.communication
 
+import ca.damocles.utilities.generateAlphaString
 import com.google.gson.Gson
 
-data class Packet(val identity: String, val side: Byte, val type: Byte, val body: HashMap<String, Any> = HashMap()){
+open class Packet(open val identity: String, val side: Byte, val type: Byte, val body: HashMap<String, Any> = HashMap()){
     override fun toString(): String {
         return Gson().toJson(this)
+    }
+}
+
+class LoginPacket(override val identity: String, username: String, password: String): Packet(side=1, type=0, identity=identity) {
+    init{
+        this.body["username"] = username
+        this.body["password"] = password
     }
 }
 
