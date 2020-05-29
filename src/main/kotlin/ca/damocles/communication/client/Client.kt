cd63a5cde8ff0bf178be12c0377ea14f82c53a23
Kowagatte <nnryanp@gmail.com>
2020-05-29 01:55:37 -0600

    Updated EstablishedConnection
    Reads incomming packets from client on seperate thread.
    Can now be disconnected
    
    An issue where one establishedconnection could halt the entire main thread exists. This issue was created here.

--- src/main/kotlin/ca/damocles/communication/client/Client.kt
+++ src/main/kotlin/ca/damocles/communication/client/Client.kt
@@ -1,15 +1,42 @@
 package ca.damocles.communication.client
 
+import ca.damocles.Server
 import ca.damocles.communication.Packet
+import ca.damocles.storage.Account
+import kotlinx.coroutines.*
 import java.io.*
 import javax.net.ssl.SSLSocket
+import kotlin.coroutines.CoroutineContext
 
-class EstablishedConnection(val connectionSocket: SSLSocket){
-    val account: Account
-    val bufferedWriter: BufferedWriter = BufferedWriter(OutputStreamWriter(connectionSocket.outputStream))
-    val bufferedReader: BufferedReader = BufferedReader(InputStreamReader(connectionSocket.inputStream))
+class EstablishedConnection(private val connectionSocket: SSLSocket){
+
+    val account: Account? = null
+    val coroutine: CoroutineContext
+    private val bufferedWriter: BufferedWriter = BufferedWriter(OutputStreamWriter(connectionSocket.outputStream))
+    private val bufferedReader: BufferedReader = BufferedReader(InputStreamReader(connectionSocket.inputStream))
 
     init{
+        coroutine = GlobalScope.launch {
+            while(true){
+                try{
+                    var line: String = getMessageFromClient()
+                    TODO("Process( Line -> Packet ) : Then -> Handle the packet.")
+                }catch(e: IOException){
+                    disconnect()
+                }
+            }
+        }
+    }
+
+    private suspend fun getMessageFromClient(): String =
+            withContext(Dispatchers.Default){
+                bufferedReader.readLine()
+            }
+
+    fun disconnect(){
+        Server.listOfEstablishedConnections.remove(this)
+        coroutine.cancel()
+        TODO("Send disconnect packet.")
     }
 
     fun send(packet: Packet){
@@ -17,4 +44,5 @@ class EstablishedConnection(val connectionSocket: SSLSocket){
         bufferedWriter.newLine()
         bufferedWriter.flush()
     }
+
 }
