a92cb5f2bbac10bacf42c768b5cb957c65633fd3
Kowagatte <nnryanp@gmail.com>
2020-06-05 11:14:56 -0600

    Documentation And Database
    AccountDatabase will now return an empty account stored on the database if no results are found, instead of null.

--- src/main/kotlin/ca/damocles/communication/client/Client.kt
+++ src/main/kotlin/ca/damocles/communication/client/Client.kt
@@ -3,14 +3,16 @@ package ca.damocles.communication.client
 import ca.damocles.Server
 import ca.damocles.communication.Packet
 import ca.damocles.storage.Account
+import ca.damocles.storage.AccountDatabase
 import kotlinx.coroutines.*
 import java.io.*
+import java.util.*
 import javax.net.ssl.SSLSocket
 import kotlin.coroutines.CoroutineContext
 
 class EstablishedConnection(private val connectionSocket: SSLSocket){
 
-    val account: Account? = null
+    val account: Account = AccountDatabase.getAccountByUUID(UUID.fromString("00000000-0000-0000-0000-000000000000"))
     private val coroutine: CoroutineContext
     private val bufferedWriter: BufferedWriter = BufferedWriter(OutputStreamWriter(connectionSocket.outputStream))
     private val bufferedReader: BufferedReader = BufferedReader(InputStreamReader(connectionSocket.inputStream))
--- src/main/kotlin/ca/damocles/storage/Account.kt
+++ src/main/kotlin/ca/damocles/storage/Account.kt
@@ -3,10 +3,16 @@ package ca.damocles.storage
 import com.google.gson.Gson
 import java.util.*
 
+/**
+ * Skeleton Account Objects used for authentication.
+ */
 data class AuthAccount(val username: String, val password: String){
     override fun toString(): String = Gson().toJson(this)
 }
 
+/**
+ * Data Class for storing Account Objects.
+ */
 data class Account(val uuid: UUID, var email: String, var username: String, var password: String): Storable{
     companion object{
         fun fromJson(accountJson: String): Account {
@@ -17,16 +23,14 @@ data class Account(val uuid: UUID, var email: String, var username: String, var 
 }
 
 /**
- * Take AuthAccount
+ * Take an AuthAccount as a parameter
  * Check if an account exists with the username of the auth account
  * if it does, compare passwords (BCrypt.checkpw)
  * return a pair, (boolean, Account) (true if it passes and the corrosponding account), ( false if it doesnt pass and null for the account)
  *
- *
  * Testing purposes:
  * username: Nick
  * password: 12345qwesd
- *
  */
 fun authenticateLogin(){
     TODO("")
