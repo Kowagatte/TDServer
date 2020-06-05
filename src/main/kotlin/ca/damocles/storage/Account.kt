9860fb2c42816bd51c3110b9b40783f83d96cbfb
Aikanwha <aikanwha@gmail.com>
2020-06-05 12:22:46 -0600

    Implemented #authenticateLogin

--- src/main/kotlin/ca/damocles/storage/Account.kt
+++ src/main/kotlin/ca/damocles/storage/Account.kt
@@ -1,7 +1,10 @@
+
 package ca.damocles.storage
 
 import com.google.gson.Gson
+import com.sun.org.apache.xpath.internal.operations.Bool
 import java.util.*
+import org.mindrot.jbcrypt.BCrypt
 
 data class AuthAccount(val username: String, val password: String){
     override fun toString(): String = Gson().toJson(this)
@@ -27,6 +30,27 @@ fun fromJson(accountJson: String): Account {
  * password: 12345qwesd
  *
  */
-fun authenticateLogin(){
-    TODO("")
+
+/*fun authenticateLogin(): Pair<Boolean,Account> {
+    TODO("Fix This.")
+    val loggingAccount = AuthAccount(username, password)
+
+    if(loggingAccount.username == AccountDatabase.getAccountByUsername(username).username)
+        if(BCrypt.checkpw(loggingAccount.password, AccountDatabase.getAccountByUsername(loggingAccount.username).password))
+            return Pair<True,>
+}
+*/
+
+
+
+fun authenticateLogin(account: AuthAccount): Pair<Boolean, Account> {
+
+    var databaseAccount = AccountDatabase.getAccountByUsername(account.username)
+    //if there's an account with this username is the database.
+    if (account.username == databaseAccount.username) {
+        //and if the password provided matches the one stored.
+        if (BCrypt.checkpw(account.password, databaseAccount.password))
+            return Pair(true, databaseAccount)
+    }
+    return Pair(false,  Account(UUID.fromString("00000000-0000-0000-0000-000000000000"), "Not Found", "Not Found", ""))
 }
