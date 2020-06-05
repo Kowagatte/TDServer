a0ab94fd1d19b1558d3426bee0497c2d0608b4e6
Aikanwha <aikanwha@gmail.com>
2020-06-05 12:25:24 -0600

    Implemented #authenticateLogin

--- src/main/kotlin/ca/damocles/storage/Account.kt
+++ src/main/kotlin/ca/damocles/storage/Account.kt
@@ -6,29 +6,34 @@ import com.sun.org.apache.xpath.internal.operations.Bool
 import java.util.*
 import org.mindrot.jbcrypt.BCrypt
 
+/**
+ * Skeleton Account Objects used for authentication.
+ */
 data class AuthAccount(val username: String, val password: String){
     override fun toString(): String = Gson().toJson(this)
 }
 
+/**
+ * Data Class for storing Account Objects.
+ */
 data class Account(val uuid: UUID, var email: String, var username: String, var password: String): Storable{
+    companion object{
+        fun fromJson(accountJson: String): Account {
+            return Gson().fromJson(accountJson, Account::class.java)
+        }
+    }
     override fun toString(): String = Gson().toJson(this)
 }
 
-fun fromJson(accountJson: String): Account {
-    return Gson().fromJson(accountJson, Account::class.java)
-}
-
 /**
- * Take AuthAccount
+ * Take an AuthAccount as a parameter
  * Check if an account exists with the username of the auth account
  * if it does, compare passwords (BCrypt.checkpw)
  * return a pair, (boolean, Account) (true if it passes and the corrosponding account), ( false if it doesnt pass and null for the account)
  *
- *
  * Testing purposes:
  * username: Nick
  * password: 12345qwesd
- *
  */
 
 /*fun authenticateLogin(): Pair<Boolean,Account> {
