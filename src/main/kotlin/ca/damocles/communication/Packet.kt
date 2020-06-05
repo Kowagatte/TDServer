f41bdb2829ec50a79024556b6187284dc9783f9e
Kowagatte <nnryanp@gmail.com>
2020-06-05 16:21:24 -0600

    Response packet and Handling
    Added the ResponsePacket
    Added abstract method #handle which does the specific packets logic

--- src/main/kotlin/ca/damocles/communication/Packet.kt
+++ src/main/kotlin/ca/damocles/communication/Packet.kt
@@ -1,22 +1,42 @@
 package ca.damocles.communication
 
-import ca.damocles.utilities.generateAlphaString
+import ca.damocles.communication.client.EstablishedConnection
+import ca.damocles.storage.authenticateLogin
 import com.google.gson.Gson
 
-open class Packet(open val identity: String, val side: Byte, val type: Byte, val body: HashMap<String, Any> = HashMap()){
+abstract class Packet(open val identity: String, val side: Byte, val type: Byte, val body: HashMap<String, Any> = HashMap()){
     companion object{
         fun fromJson(packet: String): Packet{
             return Gson().fromJson(packet, Packet::class.java)
         }
     }
+    abstract fun handle(connection: EstablishedConnection)
     override fun toString(): String {
         return Gson().toJson(this)
     }
 }
 
-class LoginPacket(override val identity: String, username: String, password: String): Packet(side=1, type=0, identity=identity) {
+class LoginPacket(override val identity: String, email: String, password: String): Packet(side=1, type=0, identity=identity) {
     init{
-        this.body["username"] = username
+        this.body["email"] = email
         this.body["password"] = password
+    }
+
+    override fun handle(connection: EstablishedConnection) {
+        val response = authenticateLogin(body["email"].toString(), body["password"].toString())
+        if(response.first){
+            connection.account = response.second
+            connection.send(ResponsePacket(identity, "Login Successful!"))
+        }else{
+            connection.send(ResponsePacket(identity, "Login Not Successful!"))
+        }
+        TODO("Not yet implemented")
+    }
+}
+
+class ResponsePacket(override val identity: String, info: String): Packet(side=0, type=0, identity=identity) {
+    override fun handle(connection: EstablishedConnection) {}
+    init{
+        this.body["info"] = info
     }
 }
