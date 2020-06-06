af560f057cdee1fdadc083ee29bad4c7492c0c8f
Kowagatte <nnryanp@gmail.com>
2020-06-05 20:06:18 -0600

    Added Packets and removed handling
    Handling done in packet classes prevents json serialization. Moved it to client class instead.
    - Added ClosePacket
    - Added Response Packet
    - Added CreateAccoutn Packet

--- src/main/kotlin/ca/damocles/communication/Packet.kt
+++ src/main/kotlin/ca/damocles/communication/Packet.kt
@@ -1,42 +1,38 @@
 package ca.damocles.communication
 
-import ca.damocles.communication.client.EstablishedConnection
-import ca.damocles.storage.authenticateLogin
 import com.google.gson.Gson
 
-abstract class Packet(open val identity: String, val side: Byte, val type: Byte, val body: HashMap<String, Any> = HashMap()){
+open class Packet(val identity: String, val side: Byte, val type: Byte, val body: HashMap<String, Any> = HashMap()){
     companion object{
         fun fromJson(packet: String): Packet{
             return Gson().fromJson(packet, Packet::class.java)
         }
     }
-    abstract fun handle(connection: EstablishedConnection)
+
     override fun toString(): String {
         return Gson().toJson(this)
     }
 }
 
-class LoginPacket(override val identity: String, email: String, password: String): Packet(side=1, type=0, identity=identity) {
+class LoginPacket(identity: String, email: String, password: String): Packet(side=1, type=0, identity=identity) {
     init{
         this.body["email"] = email
         this.body["password"] = password
     }
+}
 
-    override fun handle(connection: EstablishedConnection) {
-        val response = authenticateLogin(body["email"].toString(), body["password"].toString())
-        if(response.first){
-            connection.account = response.second
-            connection.send(ResponsePacket(identity, "Login Successful!"))
-        }else{
-            connection.send(ResponsePacket(identity, "Login Not Successful!"))
-        }
-        TODO("Not yet implemented")
+class ClosePacket(identity: String): Packet(side=1, type=1, identity = identity){}
+
+class ResponsePacket(identity: String, info: String): Packet(side=0, type=0, identity=identity) {
+    init{
+        this.body["info"] = info
     }
 }
 
-class ResponsePacket(override val identity: String, info: String): Packet(side=0, type=0, identity=identity) {
-    override fun handle(connection: EstablishedConnection) {}
+class CreateAccountPacket(identity: String, email: String, username: String, password: String): Packet(side=1, type=2, identity = identity){
     init{
-        this.body["info"] = info
+        this.body["email"] = email
+        this.body["username"] = username
+        this.body["password"] = password
     }
 }
