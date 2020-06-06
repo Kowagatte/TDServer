6849a16770944a1111554159d58abaea8ec10433
Kowagatte <nnryanp@gmail.com>
2020-06-05 20:05:13 -0600

    Packet handling done in Client
    Packet handling is now done in the EstablishedConnection class

--- src/main/kotlin/ca/damocles/communication/client/Client.kt
+++ src/main/kotlin/ca/damocles/communication/client/Client.kt
@@ -2,15 +2,17 @@ package ca.damocles.communication.client
 
 import ca.damocles.Server
 import ca.damocles.communication.Packet
+import ca.damocles.communication.ResponsePacket
 import ca.damocles.storage.Account
 import ca.damocles.storage.AccountDatabase
+import ca.damocles.storage.authenticateLogin
 import kotlinx.coroutines.*
 import java.io.*
 import java.util.*
 import javax.net.ssl.SSLSocket
 import kotlin.coroutines.CoroutineContext
 
-class EstablishedConnection(private val connectionSocket: SSLSocket){
+class EstablishedConnection(val connectionSocket: SSLSocket){
 
     var account: Account = AccountDatabase.getEmptyAccount()
     private val coroutine: CoroutineContext
@@ -19,16 +21,31 @@ class EstablishedConnection(private val connectionSocket: SSLSocket){
 
     init{
         coroutine = GlobalScope.launch {
-            while(true){
+            while(connectionSocket.isConnected){
                 try{
                     val line: String = getMessageFromClient()
-                    val incomingPacket: Packet = Packet.fromJson(line)
-                    when(incomingPacket.type){
-                        0.toByte() -> {
-                            incomingPacket.handle(this@EstablishedConnection)
+                    if(line != ""){
+                        println(line)
+                        val incomingPacket: Packet = Packet.fromJson(line)
+                        when(incomingPacket.type){
+                            0.toByte() -> {
+                                val response = authenticateLogin(incomingPacket.body["email"].toString(), incomingPacket.body["password"].toString())
+                                if(response.first){
+                                    account = response.second
+                                    send(ResponsePacket(incomingPacket.identity, "Login Successful!"))
+                                }else{
+                                    send(ResponsePacket(incomingPacket.identity, "Login Not Successful!"))
+                                }
+                            }
+                            1.toByte() -> {
+                                disconnect()
+                            }
+                            2.toByte() ->{
+                                TODO("CLIENT WANTS TO CREATE ACCOUNT")
+                            }
                         }
+                        //TODO("Process( Line -> Packet ) : Then -> Handle the packet.")
                     }
-                    TODO("Process( Line -> Packet ) : Then -> Handle the packet.")
                 }catch(e: IOException){
                     disconnect()
                 }
@@ -44,7 +61,7 @@ class EstablishedConnection(private val connectionSocket: SSLSocket){
     fun disconnect(){
         Server.listOfEstablishedConnections.remove(this)
         coroutine.cancel()
-        TODO("Send disconnect packet.")
+        //TODO("Send disconnect packet.")
     }
 
     fun send(packet: Packet){
