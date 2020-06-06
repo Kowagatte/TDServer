d1b9735818b13a16e75c5afed592b8e35f4f5a48
Kowagatte <nnryanp@gmail.com>
2020-06-05 20:04:33 -0600

    Added keystore for ssl encryption
    Also Fixed wrong credentials in launch.kt

--- src/main/kotlin/ca/damocles/Launch.kt
+++ src/main/kotlin/ca/damocles/Launch.kt
@@ -1,7 +1,7 @@
 package ca.damocles
 
 fun main(args: Array<String>) {
-    System.setProperty("javax.net.ssl.keyStore", "keystore.pfx")
-    System.setProperty("javax.net.ssl.keyStorePassword", "-~uE8@-A3Zr6Dy~%")
+    System.setProperty("javax.net.ssl.keyStore", "serverkeystore.pfx")
+    System.setProperty("javax.net.ssl.keyStorePassword", "oPkiCv43")
     Server.start()
 }
