63d89fd3874278f995e1c88a02908ccde41d35bf
Nick Ryan <nnryanp@gmail.com>
2020-07-17 23:32:23 -0600

    Moved GameMap
    GameMap now stores all relevent data that would be received from the DB. And is found in the storage package.

--- src/main/kotlin/ca/damocles/game/map/Map.kt
+++ src/main/kotlin/ca/damocles/game/map/Map.kt
@@ -1,16 +1,3 @@
 package ca.damocles.game.map
 
-import ca.damocles.storage.database.Storable
-import com.google.gson.Gson
-import java.util.*
-
 class Level(){}
-
-data class GameMap(val mapID: UUID, val name: String, val walls: List<List<Int>>, val spawns: List<Pair<Int, Int>>, val coins: List<Pair<Int, Int>>): Storable{
-    companion object{
-        fun fromJson(json: String): GameMap {
-            return Gson().fromJson(json, GameMap::class.java)
-        }
-    }
-    override fun toString(): String = Gson().toJson(this)
-}
