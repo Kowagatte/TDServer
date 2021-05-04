f8dfe43be3cf07dbaaaf1b165c8fcc2143b00c55
Nick Ryan <nnryanp@gmail.com>
2021-05-03 19:58:20 -0600

    Defragged and formatted EmailTesting.kt

--- src/main/kotlin/ca/damocles/EmailTesting.kt
+++ src/main/kotlin/ca/damocles/EmailTesting.kt
@@ -5,40 +5,55 @@ import javax.mail.internet.MimeMessage
 import java.util.Properties
 import javax.mail.*
 
+object Mail{
 
-fun main(){
-    val recipient = "nnryanp@gmail.com"
-    val subject = "Point proven"
-    val args: Array<String> = arrayOf("")
-    val fromEmail = "no-reply@damocles.ca"
-    val password = ",lujnW}6`32a8Wr|"
-    val toEmail: String = recipient
+    const val fromEmail = "no-reply@damocles.ca"
+    const val password = ",lujnW}6`32a8Wr|"
+    private val props = Properties()
 
-    val props = Properties()
-    props["mail.smtp.host"] = "mail.privateemail.com"
-    props["mail.smtp.socketFactory.port"] = "465"
-    props["mail.smtp.socketFactory.class"] = "javax.net.ssl.SSLSocketFactory"
-    props["mail.smtp.auth"] = "true"
-    props["mail.smtp.port"] = "465"
+    init{
+        props["mail.smtp.host"] = "mail.privateemail.com"
+        props["mail.smtp.socketFactory.port"] = "465"
+        props["mail.smtp.socketFactory.class"] = "javax.net.ssl.SSLSocketFactory"
+        props["mail.smtp.auth"] = "true"
+        props["mail.smtp.port"] = "465"
+    }
 
-    val auth: Authenticator = object : Authenticator() {
-        override fun getPasswordAuthentication(): PasswordAuthentication? {
+    private val auth: Authenticator = object : Authenticator() {
+        override fun getPasswordAuthentication(): PasswordAuthentication {
             return PasswordAuthentication(fromEmail, password)
         }
     }
 
-    val session = Session.getDefaultInstance(props, auth)
-    try {
-        val message: Message = MimeMessage(session)
-        message.setFrom(InternetAddress("no-reply@damocles.ca"))
-        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail))
-        message.subject = subject
-        val text: String = args.joinToString { "\n" }
-        message.setText(text)
-        Transport.send(message)
-        println("Email sent successfully")
-    } catch (e: MessagingException) {
-        println("Shit broke")
+    /**
+     * Sends an email from the configured email to the specified email,
+     * with the given subject line.
+     */
+    fun send(toEmail: String, subject: String, content: Array<String> = arrayOf("")): Boolean{
+        val session = Session.getDefaultInstance(props, auth)
+        return try {
+            val message: Message = MimeMessage(session)
+            message.setFrom(InternetAddress(fromEmail))
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail))
+            message.subject = subject
+            val text: String = content.joinToString { "\n" }
+            message.setText(text)
+            Transport.send(message)
+            true
+        } catch (e: MessagingException) {
+            false
+        }
+    }
+}
+
+fun main(){
+    val recipient = "nnryanp@gmail.com"
+    val subject = "Point proven"
+
+    if(Mail.send(recipient, subject)){
+        println("Email sent.")
+    }else{
+        println("Error Occurred.")
     }
 }
 
