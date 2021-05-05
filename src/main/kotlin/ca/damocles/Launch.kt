5df76c69bd9610a70848d4a0410cb1ec0762445f
Nick Ryan <nnryanp@gmail.com>
2021-05-05 15:49:36 -0600

    Now access keystore password through password file instead of hardcoded

--- src/main/kotlin/ca/damocles/Launch.kt
+++ src/main/kotlin/ca/damocles/Launch.kt
@@ -1,7 +1,10 @@
 package ca.damocles
 
+import ca.damocles.utilities.JsonFile
+
 fun main(args: Array<String>) {
+    val passFile = JsonFile.openJsonFile("passwords.json")
     System.setProperty("javax.net.ssl.keyStore", "serverkeystore.pfx")
-    System.setProperty("javax.net.ssl.keyStorePassword", "oPkiCv43")
+    System.setProperty("javax.net.ssl.keyStorePassword", passFile.getString("keystore"))
     Server.start()
 }
