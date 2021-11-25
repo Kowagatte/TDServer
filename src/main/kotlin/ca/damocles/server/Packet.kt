6d2f3f266e896fb27bc39febec87b62200d5b92b
Nick Ryan <nnryanp@gmail.com>
2021-11-24 19:47:54 -0600

    Update Packet.kt
    Scalped my packets to use Protobuf, instead. This Kt file just houses some test code for now. Will move to the JUnit tests later.

--- src/main/kotlin/ca/damocles/server/Packet.kt
+++ src/main/kotlin/ca/damocles/server/Packet.kt
@@ -1,84 +1,23 @@
 package ca.damocles.server
 
+import ca.damocles.packets.ClientPackets
 import ca.damocles.proto.Packets
-import ca.damocles.utilities.generateAlphaString
-import com.google.gson.Gson
-
-/**
- * PACKETS:
- * SIDE: 1=To be received by SERVER, 0=To be received by CLIENT
- * TYPE: Packets ID
- */
-open class Packet(val identity: String, val side: Byte, val type: Byte, val body: HashMap<String, Any> = HashMap()){
-    companion object{
-        fun fromJson(packet: String): Packet {
-            return Gson().fromJson(packet, Packet::class.java)
-        }
-    }
-
-    override fun toString(): String {
-        return Gson().toJson(this)
-    }
-}
+import com.google.protobuf.Any
 
 fun main(){
-    val packet = LoginPacket(generateAlphaString(3), "nnryanp@gmail.com", "12345qwesd")
-    println(packet)
-    val byteArray = Packets.ClientPacket.LoginPacket.newBuilder().setEmail("nnryanp@gmail.com").setPassword("12345qwesd").build().toByteArray()
-    println(byteArray)
-    val test = Packets.ClientPacket.LoginPacket.parseFrom(byteArray)
-    println(test.email)
-    println(test.password)
-}
+    val t = Packets.ClientPacket.LoginPacket.newBuilder().setEmail("nnryanp@gmail.com").setPassword("12345qwesd").build()
+    val ba = Any.pack(t).toByteArray()
 
-/* CLIENT PACKETS */
+    val any: Any = Any.parseFrom(ba)
 
-/**
- * Sends login credentials to be validated and signed into.
- */
-class LoginPacket(identity: String, email: String, password: String): Packet(side=1, type=0, identity=identity) {
-    init{
-        this.body["email"] = email
-        this.body["password"] = password
+    if (any.`is`(Packets.ClientPacket.LoginPacket::class.java)) {
+        println("works")
+        val specific = any.unpack(Packets.ClientPacket.LoginPacket::class.java)
+        println(specific)
     }
+
+    val spec = any.unpack(ClientPackets.inferType(any).clazz) as Packets.ClientPacket.LoginPacket
+    println("spec: $spec")
+
+
 }
-
-/**
- * Sent to tell the server the client was disconnected.
- */
-class ClosePacket(identity: String): Packet(side=1, type=1, identity = identity){}
-
-/**
- * Requests the creation of an account.
- */
-class CreateAccountPacket(identity: String, email: String, username: String, password: String): Packet(side=1, type=2, identity = identity){
-    init{
-        this.body["email"] = email
-        this.body["username"] = username
-        this.body["password"] = password
-    }
-}
-
-/**
- * Sent to tell the server the client is still responding.
- */
-class PongPacket(identity: String): Packet(side=1, type=3, identity = identity){}
-
-/* SERVER PACKETS */
-
-/**
- * Sent to client in response to other packets.
- * Includes information and a code based on the response.
- * Should be sent with the identity of the packet being responded too.
- */
-class ResponsePacket(identity: String, info: String, code: Int): Packet(side=0, type=0, identity=identity) {
-    init{
-        this.body["code"] = code
-        this.body["info"] = info
-    }
-}
-
-/**
- * Sent to check if the client is still there.
- */
-class PingPacket(identity: String): Packet(side=0, type=1, identity = identity){}
