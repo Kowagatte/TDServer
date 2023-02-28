7126fccce5bd63d680e5599b9a3cdc76d27b3153
Nicholas Ryan <nnr635@usask.ca>
2023-02-28 13:07:18 -0600

    Removed all files in master, preparing to merge orphan branch 'godot'

--- src/main/kotlin/ca/damocles/Launch.kt
+++
@@ -1,8 +0,0 @@
-package ca.damocles
-
-import ca.damocles.server.Server
-import ca.damocles.utilities.JsonFile
-
-fun main(args: Array<String>) {
-    Server.start()
-}
--- src/main/kotlin/ca/damocles/game/map/Map.kt
+++
@@ -1,5 +0,0 @@
-package ca.damocles.game.map
-
-class Level(){
-
-}
