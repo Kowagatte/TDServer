d51599418de55d44609b5e9a9bce8ae0af5bbccb
Luc <jeanluc1g88@gmail.com>
2023-03-07 22:01:33 -0700

    Coin collection implementation
    Player death and coin release still not implemented

---
+++ scripts/coin.gd
@@ -0,0 +1,22 @@
+extends Sprite2D
+
+@rpc func toggleVisiblity(_isvisible): pass
+
+@onready var area = get_node("area") as Area2D
+@onready var game = get_parent().get_parent()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	area.area_entered.connect(collided)
+
+
+
+func collided(collider):
+	if visible:
+		var playerNum = game.player_ids.find(collider.get_parent().name.to_int())
+		game.score[playerNum] += 1
+
+		for player in game.player_ids:
+			game.rpc_id(player, "updateScore", game.score)
+			rpc_id(player, "toggleVisiblity", false)
+		visible = false
