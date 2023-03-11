9f048ffc5f4ec739b285cee4dce31c487c6d6018
Nicholas Ryan <nnr635@usask.ca>
2023-03-11 12:56:23 -0600

    Store peer_id player joined the game with

--- scripts/clients.gd
+++ scripts/clients.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
 # Stores the email associated to the peer_id
-var id_to_email = {1: "test@test.co"}
+# Example structure: var id_to_email = {1: "test@test.co"}
+var id_to_email = {}
 # Stores the peer_ids (Multiple) associated with the email
-var email_to_id = {"test@test.co": [1]}
+# Example structure: var email_to_id = {"test@test.co": [1]}
+var email_to_id = {}
 # Stores the games (Multiple) associated with the email
-var games = {"test@test.co": ['012345678']}
+# Example structure: var games = {"test@test.co": {'012345678': 1}}
+var games = {}
 
 # Checks if there are any ids connected.
 func is_anyone_connected():
@@ -23,7 +26,7 @@ func add_player(id, email):
 		email_to_id[email] = [id]
 	
 	if not games.has(email):
-		games[email] = []
+		games[email] = {}
 	
 	update_counter()
 
@@ -32,7 +35,7 @@ func add_player(id, email):
 # Cannot currently unlink a game. (TDServer #15)
 func link_game(id, game):
 	if id_to_email.has(id):
-		games[id_to_email[id]].append(game)
+		games[id_to_email[id]][game] = id
 	else:
 		print("GAME ", game, " : Issue retrieving players account!")
 
