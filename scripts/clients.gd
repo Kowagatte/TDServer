2c08d2ad2e50643cb9801f8d8e3077e138c4bdad
Nicholas Ryan <nnr635@usask.ca>
2023-03-11 12:24:58 -0600

    Added multi-dicts to keep track of logged in accounts

--- scripts/clients.gd
+++ scripts/clients.gd
@@ -1,25 +1,58 @@
 extends Node2D
 
-var clients = {}
+# Stores the email associated to the peer_id
+var id_to_email = {1: "test@test.co"}
+# Stores the peer_ids (Multiple) associated with the email
+var email_to_id = {"test@test.co": [1]}
+# Stores the games (Multiple) associated with the email
+var games = {"test@test.co": ['012345678']}
 
-func _ready():
-	pass # Replace with function body.
-
+# Checks if there are any ids connected.
 func is_anyone_connected():
-	return not clients.is_empty()
+	return not id_to_email.is_empty()
 
-func add_player(id, player):
-	clients[id] = player
+# Adds the association between peer_id and email.
+# Needs to manage multiple dictionaries.
+# Would be nice to have a bi-directional table...
+func add_player(id, email):
+	id_to_email[id] = email
+	
+	if email_to_id.has(email):
+		email_to_id[email].append(id)
+	else:
+		email_to_id[email] = [id]
+	
+	if not games.has(email):
+		games[email] = []
+	
 	update_counter()
 
+# Used to link the game to the account associated with the given peer_id
+# Since peers can leave at any time, there is some issues that could arise here.
+# Cannot currently unlink a game. (TDServer #15)
+func link_game(id, game):
+	if id_to_email.has(id):
+		games[id_to_email[id]].append(game)
+	else:
+		print("GAME ", game, " : Issue retrieving players account!")
+
+# Removes peer_id
 func remove_player(id):
-	if clients.has(id):
-		clients.erase(id)
+	if id_to_email.has(id):
+		var email = id_to_email[id]
+		id_to_email.erase(id)
+		email_to_id[email].erase(id)
+		if email_to_id[email].is_empty():
+			email_to_id.erase(email)
+	
 	update_counter()
 
+# Returns number of logged in sessions. This includes users logged in on
+# 	on multiple devices/clients.
 func size():
-	return clients.size()
+	return id_to_email.size()
 
+# Updates the counter UI component
 func update_counter():
 	var label = get_parent().get_node("UI/Panel/client-count") as Label
-	label.text = String.num_int64(size())
+	label.text = str(size())
